82521917131f72e049caed0ee148bd6d0361be77
Jason <actforzjz@gmail.com>
2026-03-12 17:32:38 +0800

    Add OnlyRun plugin and C# project setup

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 * Right Align Tab Bar
 
   Aligns the tab bar of `EditorSceneTabs` and `EditorBottomPanel` to the right side of the editor window.
+
+* Only Run
+
+  Only run the project, without building it. Middle click to use console or stop running.
--- addons/utility_plugin_suite/plugin.gd
+++ addons/utility_plugin_suite/plugin.gd
@@ -3,10 +3,13 @@ extends EditorPlugin
 
 var quickly_create_node: QuicklyCreateNode = QuicklyCreateNode.new()
 var right_align_tabbar: RightAlignTabBar = RightAlignTabBar.new()
+var only_run: OnlyRun = OnlyRun.new()
 
 func _enter_tree():
 	quickly_create_node.perform(self)
 	right_align_tabbar.perform()
+	only_run.perform(self)
 
 func _exit_tree():
 	quickly_create_node.cleanup()
+	only_run.cleanup()
